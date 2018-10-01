0b668505cbe3ecb522727d676172e3ecec08df79
Michel <michel.surf26@gmail.com>
2018-09-30 23:35:39 -0300

    correcoes

---
+++ climatempo-api/src/test/java/com/ddm/climatempo/api/ClimatempoApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ddm.climatempo.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ClimatempoApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
